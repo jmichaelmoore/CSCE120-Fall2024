b24da9f3cb658764b918d8c30ba189680f544fd4
Michael Moore <jmichael@tamu.edu>
2024-09-04 14:55:10 -0500

    prep for 09/04 lecture

---
+++ 500-503/whatYouKnow/HelloWorld/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using std::string, std::cin, std::cout, std::endl;
+
+int main() {
+    string name;
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << name << ", welcome to CSCE 120." << endl;
+}
