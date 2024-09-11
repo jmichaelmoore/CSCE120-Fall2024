c88d905eac36ef1c4841fab251521e5c59ea4ebf
Michael Moore <jmichael@tamu.edu>
2024-09-11 10:58:58 -0500

    prep for 09/11

---
+++ 500-503/OOP/stopwords/start_code/stringhelper.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "stringhelper.h"
+
+using std::cout, std::cin, std::endl, std::string;
+
+size_t countString(std::string searchString, std::string source) {
+    size_t stringCount = 0;
+    return stringCount;
+}
+
+std::string removeString(std::string removeString, std::string source) {
+    size_t removeCount = 0;
+    cout << "Removed '" << removeString << "' " << removeCount << " times" << endl;
+    return source;
+}
