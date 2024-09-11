b5d52a21e2b51b47d1f4fdf2ca3bb11571dfcbf4
Michael Moore <jmichael@tamu.edu>
2024-09-11 17:39:15 -0500

    lecture 09/11 4:10

--- 500-503/design/quad.cpp
+++ 500-503/design/quad.cpp
@@ -1,5 +1,52 @@
 #include <iostream>
+#include <cmath>
 using std::cin, std::cout, std::endl;
+
+void processImaginary(double disc, double a, double b, double c) {
+    disc = fabs(disc);
+    double xa = -b/(2*a);
+    double xb = sqrt(disc)/(2*a);
+    cout << "x = " << xa << " + " << xb << "i" << endl;
+    double ckr1 = a*xa*xa - a*xb*xb + b*xa + c;
+    double cki1 = -2*xa*xb - b*xb;
+    if (fabs(ckr1 + cki1) < 0.000000001) {
+        cout << "did not work. result: " << ckr1 + cki1 << endl;
+    }
+    cout << "x = " << xa << " - " << xb << "i" << endl;
+    double ckr2 = a*xa*xa - a*-xb*-xb + b*xa + c;
+    double cki2 = -2*xa*-xb - b*-xb;
+     if (fabs(ckr2 + cki2) < 0.000000001) {
+        cout << "did not work. result: " << ckr2 + cki2 << endl;
+    }
+
+}
+
+void processReal(double disc, double a, double b, double c) {
+    double x1 = (-b + sqrt(disc))/(2*a);
+    cout << "x = " << x1 << endl;
+    double ck1 = a*x1*x1 + b*x1 + c;
+    if (fabs(ck1) > 0.000000001) {
+        cout << x1 << " did not work. result: " << ck1 << endl;
+    }
+    if (disc > 0) {
+        double x2 = (-b - sqrt(disc))/(2*a);
+        cout << "x = " << x2 << endl;
+    double ck2 = a*x2*x2 + b*x2 + c;
+    if (fabs(ck2) > 0.000000001) {
+        cout << x2 << " did not work. result: " << ck2 << endl;
+    }
+    }
+}
+
+void processQuadratic(double a, double b, double c) {
+    double disc = b*b - 4*a*c;
+    if (disc < 0) {
+        processImaginary(disc, a, b, c);
+    }
+    else {
+        processReal(disc, a, b, c);
+    }
+}
 
 void processLinear(double a, double b, double c) {
     if (b == 0) {
@@ -44,6 +91,6 @@ int main() {
         processLinear(a, b, c);
     }
     else {
-        //processQuadratic(a, b, c);
+        processQuadratic(a, b, c);
     }
 }
