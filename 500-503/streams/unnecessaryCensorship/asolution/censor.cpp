f13101728e875f81f2bc4ff4f8b7b1dfa645deca
Michael Moore <jmichael@tamu.edu>
2024-09-23 17:27:16 -0500

    lecture 4:10 09/23

--- 500-503/streams/unnecessaryCensorship/asolution/censor.cpp
+++ 500-503/streams/unnecessaryCensorship/asolution/censor.cpp
@@ -6,10 +6,6 @@
 using std::cin, std::cout, std::cerr, std::endl, std::string;
 using std::istringstream, std::ifstream, std::ofstream;
 
-string getReplace(string wd) {
-	return "***";
-}
-
 int main(int argc, char *argv[]) {
 	// use hard coded if files are not provided
 	string censorWordsFilename = "censorwords.txt";
@@ -19,17 +15,43 @@ int main(int argc, char *argv[]) {
 	string censoredTextFilename = "censored-"+originalTextFilename;
 
 	// Declare/Define/Open filestreams
+	ifstream cwi(censorWordsFilename);
+	ifstream oi(originalTextFilename);
+	ofstream co(censoredTextFilename);
 
 	// make sure all files were opened and give feedback if not
+	if (!cwi.is_open()) {
+		cout << "unable to open file: " + censoredTextFilename << endl;
+		return 1;
+	}
+	if (!oi.is_open()) {
+		cout << "unable to open file: " + originalTextFilename << endl;
+		return 1;
+	}
+	if (!co.is_open()) {
+		cout << "unable to open file: " + censoredTextFilename << endl;
+		return 1;
+	}
 
 	// get list of words to censor from file
-
+	string censorWd;
+	string censorWds;
+	while (cwi>>censorWd) {
+		censorWds += (censorWd + " ");
+	}
+	
 	// print out list of censored words (for debugging) remove later from final program
+	cout << "censorWds: " << censorWds << endl;
 
 	// read original, process and send to output
-	// process line by line
+	while (!oi.eof()) {
+		// process line by line
+		string line;
+		getline(oi, line);
 		// get next line
 		// see if each censor word is in line
 		// if it is there, replace with symbols
 		// output modified line
+		co << line << endl;
+	}
 }
