78bd3651b98a4711d1293465aacc0ac5f38962c5
Michael Moore <jmichael@tamu.edu>
2024-09-25 17:27:32 -0500

    lecture 09/25 4:10

--- 500-503/streams/unnecessaryCensorship/asolution/censor.cpp
+++ 500-503/streams/unnecessaryCensorship/asolution/censor.cpp
@@ -6,11 +6,35 @@
 using std::cin, std::cout, std::cerr, std::endl, std::string;
 using std::istringstream, std::ifstream, std::ofstream;
 
+string tolower(string str) {
+	for (size_t i=0; i<str.size(); ++i) {
+		str.at(i) = tolower(str.at(i));
+	}
+	return str;
+}
+
+string randomChar(size_t size) {
+	string str;
+	string symbols = "%$#@&!?~|";
+	for (size_t i=0; i<size; ++i) {
+		size_t index = random()%symbols.size();
+		str += symbols.at(index);
+	}
+	return str;
+}
+
 int main(int argc, char *argv[]) {
 	// use hard coded if files are not provided
 	string censorWordsFilename = "censorwords.txt";
 	string originalTextFilename = "frozen.txt";
-	
+	if (argc >= 3) {
+		originalTextFilename = argv[1];
+		censorWordsFilename = argv[2];
+	}
+	else {
+		cout << "Not enough parameters" << endl;
+		return 1;
+	}
 	// Get filenames if provided in command line arguments
 	string censoredTextFilename = "censored-"+originalTextFilename;
 
@@ -37,7 +61,7 @@ int main(int argc, char *argv[]) {
 	string censorWd;
 	string censorWds;
 	while (cwi>>censorWd) {
-		censorWds += (censorWd + " ");
+		censorWds += (tolower(censorWd) + " ");
 	}
 	
 	// print out list of censored words (for debugging) remove later from final program
@@ -48,7 +72,21 @@ int main(int argc, char *argv[]) {
 		// process line by line
 		string line;
 		getline(oi, line);
-		// get next line
+		string linelower = tolower(line);
+			// get next line
+		std::istringstream iss(censorWds);
+		while (!iss.eof()) {
+			iss >> censorWd;
+			size_t position = string::npos; 
+			do {
+				position = linelower.find(censorWd);
+				if (position != string::npos) {
+					// replace word
+					line.replace(position, censorWd.size(), randomChar(censorWd.size()));
+					linelower.replace(position, censorWd.size(), randomChar(censorWd.size()));
+				}
+			} while (position != string::npos);
+		}
 		// see if each censor word is in line
 		// if it is there, replace with symbols
 		// output modified line
