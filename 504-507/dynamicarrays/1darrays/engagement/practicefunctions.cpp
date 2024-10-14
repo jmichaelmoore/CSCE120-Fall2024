330cc0eea35bd1ef9d9ee4fd47ddbc62a2aad02e
J. Michael Moore <jmichael@tamu.edu>
2024-10-14 14:57:58 -0500

    update

---
+++ 504-507/dynamicarrays/1darrays/engagement/practicefunctions.cpp
@@ -0,0 +1,13 @@
+#include "practicefunctions.h"
+
+void makeary(char*& ary, unsigned int size) {
+}
+
+unsigned int sumary(const char* ary, unsigned int size) {
+}
+
+void deleteary(char*& ary, unsigned int& size) {
+  // deletes all memory on the heap 
+  //  and set ary and size to values representing an empty array
+
+}
