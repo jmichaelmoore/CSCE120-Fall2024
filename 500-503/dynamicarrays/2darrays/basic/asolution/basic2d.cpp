cb13437b050bb56398e79bb8bd5295dfeb18da47
J. Michael Moore <jmichael@tamu.edu>
2024-10-14 14:49:26 -0500

    prep for 10/14

---
+++ 500-503/dynamicarrays/2darrays/basic/asolution/basic2d.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using std::cin, std::cout, std::endl;
+
+// char using column major ordering
+
+void makeary(char**, unsigned int rows, unsigned int cols);
+void loadary(char**, unsigned int rows, unsigned int cols);
+void printary(char**, unsigned int rows, unsigned int cols);
+void releaseary(char**, unsigned int rows, unsigned int cols);
+
+int main() {
+  srand(time(NULL));
+
+  // could read in rows and columns, but I'll hard code for this example
+  unsigned int rows = 4;
+  unsigned int cols = 3;
+  char** ary = nullptr;
+
+  // allocate memory
+  makeary(ary, rows, cols);
+
+  // initialize
+  loadary(ary, rows, cols);
+
+  // use
+  printary(ary, rows, cols);
+
+  // release memory
+  releaseary(ary, rows, cols);
+}
+
+void makeary(char** ary, unsigned int rows, unsigned int cols) {
+  // declare variable (done in function signature)
+
+  // create array of pointers for cols
+
+}
+
+void loadary(char** ary, unsigned int rows, unsigned int cols) {
+  // can have col or row be the outer loop, it doesn't matter
+  for (unsigned int col=0; col<cols; col++) {
+    for (unsigned int row=0; row<rows; row++) {
+      char val;
+      if (rand()%2) { // uppercase
+        val = rand()%26 + 'A';
+      }
+      else { // lowercase
+        val = rand()%26 + 'a';
+      }
+      ary[col][row] = val;
+    }
+  }
+}
+
+void printary(char** ary, unsigned int rows, unsigned int cols) {
+  // must print with row in outer loop to match our mental model
+  for (unsigned int row=0; row<rows; row++) {
+    for (unsigned int col=0; col<cols; col++) {
+      cout << ary[col][row] << " ";
+    }
+    cout << endl;
+  }
+}
+
+void releaseary(char** ary, unsigned int rows, unsigned int cols) {
+}
