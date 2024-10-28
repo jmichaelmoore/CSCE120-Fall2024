bd658cc9e6ed7db0f0bfb7d88b7f15cb03cd6dda
Michael Moore <jmichael@tamu.edu>
2024-10-28 15:19:44 -0500

    prep 10/28

---
+++ 500-503/classes/student/code-org/driver.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Student.h"
+
+using std::cout, std::endl;
+
+int main(){
+
+  auto Student1 = Student("Alice", 1234);
+  // Student Student1("Alice", 1234);
+  auto Student2 = Student("Bob");
+  // Student Student2("Bob");
+  Student Student3 = Student(5678);
+  // Student Student3(5678);
+  Student Student4 = Student();
+  // Student Student4;
+
+  cout << "My name is " << Student1.getName() << "and my UIN is" << Student1.uin << endl;
+  cout << "My name is " << Student2.getName() << "and my UIN is" << Student2.uin << endl;
+  cout << "My name is " << Student3.name << "and my UIN is" << Student3.uin << endl;
+  cout << "My name is " << Student4.name << " and my UIN is " << Student4.uin << endl;
+}
---
+++ 500-503/classes/student/const/Student.h
@@ -0,0 +1,21 @@
+#include <string>
+
+class Student {
+  
+  private:
+    
+    std::string name;
+    int uin;
+  
+  public:
+    
+    //Constructors
+    Student(std::string name, int uin);
+    Student(std::string name);
+    Student(int uin);
+    Student();
+
+    //Methods
+    std::string getName() const { return name; }
+    int getUIN() const { return uin; }
+};
