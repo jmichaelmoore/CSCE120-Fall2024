a943f0208dae20bc0bcb3b00154e30599e584deb
J. Michael Moore <jmichael@tamu.edu>
2024-11-04 15:47:07 -0600

    prep for 11/4

---
+++ 500-503/classeswithdynamicmemory/dynamicarray/start/DynamicIntArray.h
@@ -0,0 +1,11 @@
+#ifndef DYNAMICARRAY_H
+#define DYNAMICARRAY_H
+
+#include<iostream>
+
+class DynamicIntArray {
+public:
+
+};
+
+#endif
---
+++ 504-507/classeswithdynamicmemory/dynamicarray/start/driver.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "DynamicIntArray.h"
+
+using namespace std;
+
+void printArray(string label, const DynamicIntArray& ary);
+
+int main() {
+  // declare/define instance
+  DynamicIntArray a;
+  printArray("a", a);
+
+/*
+  DynamicIntArray b(3, 7);
+  printArray("b", b);
+
+  DynamicIntArray c(5);
+  printArray("c", c);
+
+  c.at(2) = 2;
+  c[0] = 9;
+
+  printArray("c", c);
+
+  b.push_back(1);
+
+  printArray("b", b);
+
+  DynamicIntArray d(b);
+
+  printArray("d", d);
+
+  a = d;
+
+  printArray("a",a);
+
+  a.at(a.size() -1) = 11;
+
+  printArray("d",d);
+  printArray("a", a);
+  */
+}
+
+void printArray(string label, const DynamicIntArray& ary) {
+  cout << label << endl;
+}
