dca5d8d1ef6426a91d9347e41e63f0094c3591da
Michael Moore <jmichael@tamu.edu>
2024-11-13 16:23:56 -0600

    ll solution 500-503

--- 500-503/linkedlist/int/aSolution/LLint.cpp
+++ 500-503/linkedlist/int/aSolution/LLint.cpp
@@ -3,9 +3,25 @@
 using std::cout, std::endl;
 
 void LLint::insertFront(const int& data) {
+    Node* newNode = new Node(data);
+    if (head == nullptr) {
+        tail = newNode;
+    }
+    else {
+        newNode->next = head;
+    }
+    head = newNode;
 }
 
 void LLint::insertBack(const int& data) {
+    Node* newNode = new Node(data);
+    if (tail == nullptr) {
+        head = newNode;
+    }
+    else {
+        tail->next = newNode;
+    }
+    tail = newNode;
 }
 
 void LLint::insertAfter(const int& valToInsert, const int& valToFind) {
@@ -15,7 +31,16 @@ void LLint::insertBefore(const int& valToInsert, const int& valToFind) {
 }
 
 void LLint::print(std::ostream& os) const {
+    Node* cur = head;
+    while (cur != nullptr) {
+        cout << cur->data << endl;
+    }
 }
 
 void LLint::printReverse(std::ostream& os) {
+}
+
+std::ostream& operator<<(std::ostream& os, const LLint& ll) {
+    ll.print(os);
+    return os;
 }
